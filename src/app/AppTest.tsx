e5855f9e9cb695799ee3d030928e75a90c388d58
YuliiaKvitka <modernwebsite21@gmail.com>
2024-02-26 18:25:40 +0200

    create front

---
+++ src/app/AppTest.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const AppTest = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl text-red-500" >🍕 Pizza</h1>
+    </div>
+  )
+}
+
+export default AppTest
